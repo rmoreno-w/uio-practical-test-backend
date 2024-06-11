7ce87f9c0709ef3669247a94969b189e21c03f61
Rodrigo Moreno <rmoreno.w@gmail.com>
2024-06-11 20:53:03 -0300

    feat: Adding user finding by email method

--- people/people.py
+++ people/people.py
@@ -6,16 +6,54 @@ people = Blueprint("people", __name__)
 
 # Lists all of the current users
 @people.route("/", methods=["GET"])
-def people_home():
+def list_users():
     current_folder_path = os.path.realpath(os.path.dirname(__file__))
     json_uri = os.path.join(current_folder_path, "people_list.json")
     
-    with open(json_uri, 'r') as json_file:
-        json_data = json.load(json_file)
-        print(type(json_data))
+    try:
+        with open(json_uri, 'r') as json_file:
+            json_data = json.load(json_file)
+            print(type(json_data))
+    # If the file with the data is not found
+    except:
+        json_data = {
+            "data": []
+        }
+        return jsonify(json_data), 400
     
     # Removing password field for each person found in order to not send password
     for person in json_data["data"]:
         del person["password"]
 
     return jsonify(json_data), 200
+
+
+# Query user by email
+@people.route("/<email>", methods=["GET"])
+def find_user(email):
+    current_folder_path = os.path.realpath(os.path.dirname(__file__))
+    json_uri = os.path.join(current_folder_path, "people_list.json")
+    
+    try:
+        with open(json_uri, 'r') as json_file:
+            json_data = json.load(json_file)
+            print(type(json_data))
+    # If the file with the data is not found
+    except:
+        json_data = {
+            "data": []
+        }
+        return jsonify(json_data), 400
+    
+    # Removing password field for each person found in order to not send password
+    for person in json_data["data"]:
+        if person["email"] == email:
+            del person["password"]
+            return jsonify(person), 200
+        
+    #If the search didnt return any person with that email
+    return jsonify({"message": "Could not find an user with the email provided"}), 400
+
+
+
+
