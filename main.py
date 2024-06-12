dc1809d5d802b57d8ee95642b9133460ac417da8
Rodrigo Moreno <rmoreno.w@gmail.com>
2024-06-12 01:27:29 -0300

    build: Adding flask_cors to enable CORS api calls

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
 from calculate.calculate import calculate
 from people.people import people
 
 app = Flask(__name__)
+CORS(app)
+
 app.register_blueprint(people, url_prefix='/people')
 app.register_blueprint(calculate, url_prefix='/calculate')
 
