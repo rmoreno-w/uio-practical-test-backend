bfdd45f55ad94a5e0791b1dca9fcca18b8325d90
Rodrigo Moreno <rmoreno.w@gmail.com>
2024-06-12 04:41:42 -0300

    fix: Receiving Year field in Calculate Page and change calc

--- calculate/calculate.py
+++ calculate/calculate.py
@@ -11,8 +11,8 @@ folder_above_path = os.path.abspath(os.path.join(current_folder, os.pardir))
 
 json_uri = os.path.join(folder_above_path, 'people', "people_list.json")
 
-@calculate.route('/', methods=['GET'])
-def calculate_average_age():
+@calculate.route('/<int:year>', methods=['GET'])
+def calculate_average_age(year):
     try:
         with open(json_uri, 'r') as json_file:
             json_data = json.load(json_file)
@@ -31,9 +31,16 @@ def calculate_average_age():
         today_date = date.today()
         year_of_birth = date(int(person["year_of_birth"]), 1, 1)
 
-        delta = today_date - year_of_birth
-        difference_in_years = math.floor(delta.days / 365)
-        age_list.append(difference_in_years)
+        if year == int(person["year_of_birth"]):
+            print(person)
 
-    average_age = sum(age_list) / len(age_list)
-    return jsonify({"average_age": average_age}), 200
+            delta = today_date - year_of_birth
+            difference_in_years = math.floor(delta.days / 365)
+            age_list.append(difference_in_years)
+
+    if len(age_list) != 0:
+        average_age = sum(age_list) / len(age_list)
+        return jsonify({"average_age": average_age}), 200
+    else:
+        average_age = 0
+        return jsonify({"message": f"There were no users found born in the year of {year}", "average_age": average_age}), 400
