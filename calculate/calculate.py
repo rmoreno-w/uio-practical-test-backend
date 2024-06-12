a11965bcce0d8c9000373649d6079692487d91fa
Rodrigo Moreno <rmoreno.w@gmail.com>
2024-06-11 23:15:29 -0300

    feat: Adding calculate module and age calc method

---
+++ calculate/calculate.py
@@ -0,0 +1,39 @@
+import math
+import os
+from datetime import date
+
+from flask import Blueprint, json, jsonify, request
+
+calculate = Blueprint("calculate", __name__)
+
+current_folder = os.path.realpath(os.path.dirname(__file__))
+folder_above_path = os.path.abspath(os.path.join(current_folder, os.pardir))
+
+json_uri = os.path.join(folder_above_path, 'people', "people_list.json")
+
+@calculate.route('/', methods=['GET'])
+def calculate_average_age():
+    try:
+        with open(json_uri, 'r') as json_file:
+            json_data = json.load(json_file)
+    except:
+        json_data = {
+            "average_age": "No users in the database yet"
+        }
+        return jsonify(json_data), 400
+    
+
+    if len(json_data["data"]) == 0:
+        return jsonify({"average_age": "No users in the database yet"}), 400
+
+    age_list = []
+    for person in json_data["data"]:
+        today_date = date.today()
+        year_of_birth = date(int(person["year_of_birth"]), 1, 1)
+
+        delta = today_date - year_of_birth
+        difference_in_years = math.floor(delta.days / 365)
+        age_list.append(difference_in_years)
+
+    average_age = sum(age_list) / len(age_list)
+    return jsonify({"average_age": average_age}), 200
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask
 
+from calculate.calculate import calculate
 from people.people import people
 
 app = Flask(__name__)
 app.register_blueprint(people, url_prefix='/people')
+app.register_blueprint(calculate, url_prefix='/calculate')
 
 if __name__ == '__main__':
     app.run(debug=True)
